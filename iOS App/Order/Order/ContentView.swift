7b22019846924d454ccb46300df9f9318ae45ee8
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-12-11 01:50:15 +0200

    Created the Project for the mobile app

---
+++ iOS App/Order/Order/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Order
+//
+//  Created by Ioannis Loudaros on 11/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
