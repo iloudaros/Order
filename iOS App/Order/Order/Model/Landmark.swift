cb7c320ff60c241bf133292b988599a085580023
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-12-11 23:56:52 +0200

    Ready for beginning chapter "Handling User Input"
    Όλα πάνε πολύ καλά κυρία Νίτσα μου <3

--- iOS App/Order/Order/Model/Landmark.swift
+++ iOS App/Order/Order/Model/Landmark.swift
@@ -1,35 +1,28 @@
-//
-//  File.swift
-//  Order
-//
-//  Created by Ioannis Loudaros on 11/12/21.
-//
-
 import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
-    
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-    
+
     private var coordinates: Coordinates
-    
-    var locationCoordinate: CLLocationCoordinate2D{
-        CLLocationCoordinate2D(latitude: coordinates.latitude,
-                               longitude: coordinates.longtitude)
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
     }
-    
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
-        var longtitude: Double
+        var longitude: Double
     }
 }
---
+++ iOS App/Order/Order/Views/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  Order
+//
+//  Created by Ioannis Loudaros on 11/12/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+            .previewDevice(PreviewDevice(rawValue:deviceName))
+            .previewDisplayName(deviceName)
+    }
+}
+}
