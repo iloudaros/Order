3fc335ffe74084cee495709032f8f652c57646b2
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-12-12 06:27:43 +0200

    Started Building Order Mobile App

--- iOS App/Order/Order/Views/ContentView.swift
+++ iOS App/Order/Order/Views/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LandmarkList()
-            
+        VStack{
+            Spacer()
+            Image("LogoLetters")
+            Spacer()
+            VStack{
+                TextField("Username", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                SecureField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("Apple")/*@END_MENU_TOKEN@*/)
+            }
+            Spacer()
         }
-            
+        
     }
+    
+}
 
 
 struct ContentView_Previews: PreviewProvider {
